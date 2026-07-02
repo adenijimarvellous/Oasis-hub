59e1f7f32e0c79b88d0c8c0a85649fb24f61359c
adenijimarvellous <adenijimarvellous33@gmail.com>
2026-07-02 10:38:31 +0100

    Set up cabin data fetching with Supabase
    Create apiCabins service module to fetch cabin data from Supabase. Integrate data fetching in Cabins component using useEffect hook. Add Supabase credentials to environment configuration and display test cabin image from Supabase storage.

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,11 +1,27 @@
+import { useEffect } from "react";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
+import { getCabins } from "../services/apiCabins";
 
 function Cabins() {
+  useEffect(() => {
+    async function fetchCabins() {
+      try {
+        const cabins = await getCabins();
+        console.log(cabins);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    fetchCabins();
+  }, []);
+
   return (
     <Row type="horizontal">
       <Heading as="h1">All cabins</Heading>
       <p>TEST</p>
+      <img src="https://vzqgywnfcfthptvwvxpl.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg" />
     </Row>
   );
 }
