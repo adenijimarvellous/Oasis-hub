7d75986a28506da2d94cd429da5cc1d913f998ac
adenijimarvellous <adenijimarvellous33@gmail.com>
2026-07-06 09:39:46 +0100

    Integrate react-hook-form and fix Button prop naming
    Add react-hook-form dependency and refactor CreateCabinForm to use form validation hooks. Update Button component to use transient props ($size, $variation) to avoid passing styling props to DOM. Add UI toggle in Cabins page to show/hide cabin creation form.

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,9 +1,14 @@
+import { useState } from "react";
+
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
-
 import CabinTable from "../features/cabins/CabinTable";
+import CreateCabinForm from "../features/cabins/CreateCabinForm";
+import Button from "../ui/Button";
 
 function Cabins() {
+  const [showForm, setShowForm] = useState(false);
+
   return (
     <>
       <Row type="horizontal">
@@ -11,8 +16,17 @@ function Cabins() {
         <p>Filter / Sort</p>
       </Row>
 
-      <Row>
+      <Row type="vertical">
         <CabinTable />
+
+        <Button
+          onClick={() => setShowForm((show) => !show)}
+          $variation="primary"
+          $size="medium"
+        >
+          Add New Cabin
+        </Button>
+        {showForm && <CreateCabinForm />}
       </Row>
     </>
   );
