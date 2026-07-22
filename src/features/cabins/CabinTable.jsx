f1e499a9ca9ca87692d0c06356241ea1d52c1170
adenijimarvellous <adenijimarvellous33@gmail.com>
2026-07-22 10:14:12 +0100

    Add sorting functionality to cabin table
    Implement a new SortBy component that manages sorting via URL parameters, with options to sort cabins by name, price, and capacity. Add a reusable Select component for dropdown inputs. Integrate sorting logic into CabinTable and improve filter UX by disabling the currently active filter button.

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -39,6 +39,7 @@ function CabinTable() {
     return <Spinner />;
   }
 
+  //1. FILTER
   const filterValue = searchParams.get("discount") || "all";
 
   let filteredCabins;
@@ -51,6 +52,14 @@ function CabinTable() {
   if (filterValue === "with-discount") {
     filteredCabins = cabins.filter((cabin) => cabin.discount > 0);
   }
+
+  //2. SORT
+  const sortBy = searchParams.get("sortBy") || "startDate-asc";
+  const [field, direction] = sortBy.split("-");
+  const modifier = direction === "asc" ? 1 : -1;
+  const sortedCabins = filteredCabins.sort(
+    (a, b) => (a[field] - b[field]) * modifier,
+  );
 
   return (
     <Menus>
@@ -66,7 +75,8 @@ function CabinTable() {
 
         <Table.Body
           // data={cabins}
-          data={filteredCabins}
+          // data={filteredCabins}
+          data={sortedCabins}
           render={(cabin) => <CabinRow cabin={cabin} key={cabin.id} />}
         />
       </Table>
